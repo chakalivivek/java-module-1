474861716528357314c00771e184529a6ea4e619
c.vivek <vivekchakali1705@gmail.com>
2025-08-26 15:05:37 +0530

    Add files via upload

---
+++ squares.java
@@ -0,0 +1,9 @@
+public class squares {
+    public static void main(String[] args) {
+        int num1 = 4;
+        
+        int square1 = num1 * num1;
+        System.out.println("Square of " + num1 + " is: " + square1);
+        
+    }
+}
